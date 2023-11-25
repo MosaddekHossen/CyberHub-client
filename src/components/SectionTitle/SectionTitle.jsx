7bf1a65d6092f5fccc44b33cd891cc0348ade6c0
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-11-26 03:32:57 +0600

    added extra section

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ const SectionTitle = ({ heading, subHeading }) => {
     return (
         <div className="text-center py-10 space-y-3 md:mt-7 mt-4 px-8 lg:px-0">         
             <h2 className="md:text-5xl text-3xl text-[#262525] font-bold">{heading}</h2>
-            <h3 className="text-2xl font-bold text-[#141414]">{subHeading}</h3>
+            <h3 className="md:text-2xl text-[16px] font-bold text-[#141414]">{subHeading}</h3>
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,10 @@ import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Partners from "../Partners/Partners";
 import Teaching from "../Teaching/Teaching";
+import Company from "../Company/Company";
+import Users from "../Users/Users";
+import Stay from "../Stay/Stay";
+import Slider from "../Slider/Slider";
 
 const Home = () => {
     return (
@@ -12,11 +16,15 @@ const Home = () => {
             </Helmet>
             <div className="max-w-screen-xl mx-auto">
                 <Banner></Banner>
+                <Company></Company>
+                <Users></Users>
                 <Partners></Partners>
                 <Category></Category>
                 <div className="mt-60">
                     <Teaching></Teaching>
                 </div>
+                <Stay></Stay>
+                <Slider></Slider>
             </div>
         </div>
     );
